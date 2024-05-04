8123dbd098c43ce63b4d2172c4ee6a6ffe7c6c0e
hung1720 <coderlazy17@gmail.com>
2024-05-04 13:07:29 +0700

    update src code

--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
-const express = require('express')
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
 
-const app = express();
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
+
+const app = express()
 
 const hostname = 'localhost'
-const port = 3000;
-app.get('/', function (req, res){
-    res.send('<h1>Hello world</h1>')
+const port = 8017
+
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(mapOrder(
+    [ { id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' } ],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
 })
